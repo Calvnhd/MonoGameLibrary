f4646a984864e3dcb68c81ec9ba70af6246c2f92
Calvin Davidson <82812675+Calvnhd@users.noreply.github.com>
2026-01-29 02:24:20 +1100

    add animation switching to AnimatedSprite.cs

--- Graphics/AnimatedSprite.cs
+++ Graphics/AnimatedSprite.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 
 namespace MonoGameLibrary.Graphics;
@@ -8,6 +9,7 @@ public class AnimatedSprite : Sprite
     private int _currentFrame;
     private TimeSpan _elapsed;
     private Animation _animation;
+    private Dictionary<string, Animation> _animations = new();
 
     /// <summary>
     /// Gets or Sets the animation for this animated sprite.
@@ -21,6 +23,12 @@ public class AnimatedSprite : Sprite
             Region = _animation.Frames[0];
         }
     }
+
+    /// <summary>
+    /// Gets the name of the currently playing animation, or null if set directly via the Animation property.
+    /// </summary>
+    public string CurrentAnimationName { get; private set; }
+
     /// <summary>
     /// Creates a new animated sprite.
     /// </summary>
@@ -33,6 +41,37 @@ public class AnimatedSprite : Sprite
     public AnimatedSprite(Animation animation)
     {
         Animation = animation;
+    }
+
+    /// <summary>
+    /// Adds an animation with the specified name.
+    /// </summary>
+    /// <param name="name">The name to identify this animation.</param>
+    /// <param name="animation">The animation to add.</param>
+    public void AddAnimation(string name, Animation animation)
+    {
+        _animations[name] = animation;
+    }
+
+    /// <summary>
+    /// Sets the current animation by name. If the animation is already playing, it will not restart.
+    /// </summary>
+    /// <param name="name">The name of the animation to play.</param>
+    /// <returns>True if the animation was changed, false if already playing or not found.</returns>
+    public bool SetAnimation(string name)
+    {
+        if (!_animations.TryGetValue(name, out var animation))
+            return false;
+
+        if (animation == _animation)
+            return false;
+
+        _animation = animation;
+        _currentFrame = 0;
+        _elapsed = TimeSpan.Zero;
+        Region = _animation.Frames[0];
+        CurrentAnimationName = name;
+        return true;
     }
     /// <summary>
     /// Updates this animated sprite.
